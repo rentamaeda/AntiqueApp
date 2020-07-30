330abbdf823614fc3e7a0612d0cf01eb867fc68b
maedarenfutoshi <japan_lunch_fate@yahoo.co.jp>
2020-07-30 22:05:43 +0900

    アカウント作成できない

--- AntiqueApp/TabBarController.swift
+++ AntiqueApp/TabBarController.swift
@@ -9,7 +9,7 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
         // タブバーの背景色
         self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
-        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
+        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する、
         self.delegate = self
     }
 
